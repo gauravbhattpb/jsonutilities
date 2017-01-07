ad3c034a874096f7102c59f552e97d8ad16148a5
gauravbhattpb <gaurav.bhatt@pb.com>
2017-01-07 13:15:47 +0530

    Removed flips entry

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var flips = require('./routes/flips');
 
 var app = express();
 
